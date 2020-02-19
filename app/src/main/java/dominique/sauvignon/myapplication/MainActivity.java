8dd85bf9e9e8cc1143db57aad39e28d471f40efc
Dominique SAUVIGNON <dominique21000.pro@gmail.com>
2020-02-19 21:50:01 +0100

    initial commit

---
+++ app/src/main/java/dominique/sauvignon/myapplication/MainActivity.java
@@ -0,0 +1,130 @@
+package dominique.sauvignon.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+
+
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Bundle;
+import android.util.Size;
+import android.view.TextureView;
+import android.widget.TextView;
+
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextureView textureView;
+
+    CameraDevice cameraDevice;
+    String cameraId;
+    Size imageDimensions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textureView = (TextureView) findViewById(R.id.texture);
+        textureView.setSurfaceTextureListener(surfaceTextureListener);
+
+
+        /*
+        // on verifie qu'il y a une camera
+        boolean c_ok = checkCameraHardware(this);
+
+        if (c_ok)
+        {
+            // camera ok
+        */
+
+/*
+        }
+        else{
+            // ceamera ko
+        }
+    */
+    }
+
+    TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    private void openCamera() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        cameraId = cameraManager.getCameraIdList()[0];
+
+        CameraCharacteristics cc = cameraManager.getCameraCharacteristics(cameraId);
+        StreamConfigurationMap map = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        imageDimensions = map.getOutputSizes(SurfaceTexture.class)[0];
+
+        if (ActivityCompat.checkSelfPermission((this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+
+            return ;
+        }
+        cameraManager.openCamera(cameraId, stateCallback, null);
+    }
+    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            startCameraPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+
+
+
+}
